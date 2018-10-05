80258ceda3911943df2b5bf7f65904d5b9c629a5
Zainde <42198931+Zainde@users.noreply.github.com>
2018-10-05 09:56:22 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
  */
 if('serviceWorker' in navigator) {
     window.addEventListener("load", () => {
-        navigator.serviceWorker.register('.../serviceworker.js')
+        navigator.serviceWorker.register('/serviceworker.js')
             .then(registration => {
                 console.log("Service Worker registered");
             })
