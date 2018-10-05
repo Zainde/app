1f490019c995c80d86492463cf9a16b6f9785d5a
Zainde <42198931+Zainde@users.noreply.github.com>
2018-10-05 10:30:12 +0200

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-/**
+/*
  * Installer Service Worker
  * Lytter efter install event og tilføjer App Shell filer til cache
  */
@@ -6,6 +6,7 @@ self.addEventListener("install", function(e) {
     console.log("Service Worker Installed");
     e.waitUntil(
         caches.open('AppTest').then(function(cache) {
+          console.log("test1");
             return cache.addAll([
                 '/',
                 '/index.html',
@@ -16,14 +17,14 @@ self.addEventListener("install", function(e) {
     );
 });
 
-/**
+/*
  * Aktiver Service Worker
  */
 self.addEventListener("activate", function(event) {
     console.log("Service Worker Activated");
 });
 
-/**
+/*
  * Service Worker Fetch
  * Tjek cache efter match og returner hvis der er et match
  * Ellers hent fil via netværk og tilføj til cache
